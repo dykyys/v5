e69d8930752a818685d3df9ec4284d2457b01fee
dykyys <56254283+dykyys@users.noreply.github.com>
2024-04-27 21:33:52 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import PAGE_NAMES from "./router/paths";
 
-
+import HeaderLayout from "./layouts/HeaderLayout/HeaderLayout";
 
 export const App = () =>{
   return (
   
      
      <Routes>
-        <Route path="/" element={ <h1>home</h1>}>
+        <Route path="/" element={ <HeaderLayout/>}>
           <Route index element={ <h1>home</h1>} />
             </Route>
       </Routes>
