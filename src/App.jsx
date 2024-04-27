e16bf4833ad1c49ef65f2db9d32eec861614cc3c
Sergii <dykyy.s@gmail.com>
2024-04-27 21:46:15 +0300

    del

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,10 @@ import HeaderLayout from './layouts/HeaderLayout/HeaderLayout';
 
 export const App = () => {
   return (
-    <>
-      <HeaderLayout />
-      <Routes>
-        <Route path="/" element={<h1>home</h1>}></Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<HeaderLayout />}>
+        <Route index element={<h1>home</h1>} />
+      </Route>
+    </Routes>
   );
 };
