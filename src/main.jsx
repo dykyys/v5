19d767b017f3b65133ad8a3cdb9f038dc0aba042
Sergii <dykyy.s@gmail.com>
2024-04-27 21:48:38 +0300

    del

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-import {App} from "./App.jsx";
-// import { LoadingProvider } from "./components/LoadingProvider/LoadingProvider.jsx";
+import { App } from './App.jsx';
 
-import "./index.css";
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <LoadingProvider> */}
       <App />
-      {/* </LoadingProvider> */}
     </BrowserRouter>
   </React.StrictMode>,
 );
