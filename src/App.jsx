34c818c78824cfe50de1c31747ee387cb804b1d4
dykyys <56254283+dykyys@users.noreply.github.com>
2024-04-27 21:36:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { lazy } from "react";
+
 import { Route, Routes } from "react-router-dom";
-import PAGE_NAMES from "./router/paths";
+
 
 import HeaderLayout from "./layouts/HeaderLayout/HeaderLayout";
 
