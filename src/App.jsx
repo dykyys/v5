1c4746e80056edfb25c6d5bd2d6921936dae57f7
dykyys <56254283+dykyys@users.noreply.github.com>
2024-04-27 21:18:04 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import PAGE_NAMES from "./router/paths";
 
 import HeaderLayout from "./layouts/HeaderLayout/HeaderLayout";
-import { Toaster } from "react-hot-toast";
+
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
 const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage.jsx"));
@@ -16,29 +16,20 @@ const NotFoundPage = lazy(
   () => import("./pages/NotFoundPage/NotFoundPage.jsx"),
 );
 
-function App() {
+export export App = () =>{
   return (
-    <>
+  
      
       <Routes>
-        <Route path={PAGE_NAMES.homepage} element={ <HeaderLayout />}>
+        <Route path={'/'} element={ <HeaderLayout />}>
           
            <Route index element={ <h1>home</h1>}/>
-          <Route path={PAGE_NAMES.movies} element={<h1>movies</h1>} />
+          <Route path={'movies'} element={<h1>movies</h1>} />
 
-          <Route path={PAGE_NAMES.movieDetails} element={<MovieDetailsPage />}>
-            <Route path={PAGE_NAMES.cast} element={<Cast />} />
-            <Route path={PAGE_NAMES.reviews} element={<Reviews />} />
-          </Route>
- </Route>
-          <Route path="*" element={<NotFoundPage />} />
-     
-        
       </Routes>
 
-     
-    </>
+  
   );
 }
 
-export default App;
+
