d072bd172f55b5d7d0a132743a0c4a75a2a8776d
Sergii <dykyy.s@gmail.com>
2024-04-27 21:45:20 +0300

    del

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 
-import { Route, Routes } from "react-router-dom";
+import HeaderLayout from './layouts/HeaderLayout/HeaderLayout';
 
-
-import HeaderLayout from "./layouts/HeaderLayout/HeaderLayout";
-
-export const App = () =>{
+export const App = () => {
   return (
-  <><HeaderLayout/>
-     <Routes>
-        <Route path="/" element={<h1>home</h1> }>
-        
-            </Route>
-      </Routes></>
-     
-  
+    <>
+      <HeaderLayout />
+      <Routes>
+        <Route path="/" element={<h1>home</h1>}></Route>
+      </Routes>
+    </>
   );
-}
-
-
+};
