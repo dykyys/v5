11b0eb6bc7c97887f1d072aea3c61b125dae3b7d
dykyys <56254283+dykyys@users.noreply.github.com>
2024-04-27 21:30:10 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,17 @@ import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import PAGE_NAMES from "./router/paths";
 
-import HeaderLayout from "./layouts/HeaderLayout/HeaderLayout";
 
-
-const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
-const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage.jsx"));
-const MovieDetailsPage = lazy(
-  () => import("./pages/MovieDetailsPage/MovieDetailsPage.jsx"),
-);
-const Cast = lazy(() => import("./components/Cast/Cast"));
-const Reviews = lazy(() => import("./components/Reviews/Reviews"));
-const NotFoundPage = lazy(
-  () => import("./pages/NotFoundPage/NotFoundPage.jsx"),
-);
 
 export const App = () =>{
   return (
   
      
-      <h1>home</h1>
+     <Routes>
+        <Route path="/" element={ <h1>home</h1>}>
+          <Route index element={ <h1>home</h1>} />
+          
+      </Routes>
   
   );
 }
